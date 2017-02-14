869d0291ecabfac01f0808ed5306e348e1025a37
arkatebi <arkatebi@gmail.com>
2017-02-14 14:59:34 -0500

    Update stSwitch-pSet-1.py

--- toggleSwitch/stSwitch-pSet-1.py
+++ toggleSwitch/stSwitch-pSet-1.py
@@ -21,7 +21,26 @@
 '''
 import sys
 import random
+from collections import OrderedDict
+import auxiliary_functions as aux
 
+#-----------------------------------------------------------------------------#
+def defineSystem():
+    '''
+    Create dictionaries to define the system
+    '''
+    #set parameters:
+    pars = OrderedDict() 
+    pars = aux.parameter_st_1()
+    #set initial conditions for the system variables: 
+    vars=OrderedDict()
+    vars['X'] = 100 
+    vars['Y'] = 100
+
+    #set simulation time: 
+    tmax=5.0e+6 
+    return (pars,vars,tmax)  
+    
 #-----------------------------------------------------------------------------#
 def shifted_hill_impact(X, X0, nX, lamb):
     '''
@@ -30,50 +49,52 @@ def shifted_hill_impact(X, X0, nX, lamb):
     return lamb + (1.0-lamb)/(1.0+(X/X0)**nX)
 
 #-----------------------------------------------------------------------------#
-def calculate_propensities(X0,Y0,
-                           gX,gY,
-                           kX,kY,
-                           X,Y,
-                           nX,nY,
-                           lX,lY):
-    sh_impact_X = shifted_hill_impact(X, X0, nX, lX)
-    sh_impact_Y = shifted_hill_impact(Y, Y0, nY, lY)
-    #pR1 = gX*sh_impact_Y*X
-    pR1 = gX*sh_impact_Y
-    pR2 = kX*X
-    #pR3 = gY*sh_impact_X*Y
-    pR3 = gY*sh_impact_X
-    pR4 = kY*Y
-    return pR1,pR2,pR3,pR4
+def calculate_propensities(pars, vars):
+    '''
+    Calculates propensitity for each reaction.
+    '''
+    #create an empty list to store propensities:
+    pros = list()
+    sh_impact_X = shifted_hill_impact(vars.get('X'), 
+                                      pars.get('X0'), 
+                                      pars.get('nX'), 
+                                      pars.get('lX'))
+    sh_impact_Y = shifted_hill_impact(vars.get('Y'), 
+                                      pars.get('Y0'), 
+                                      pars.get('nY'), 
+                                      pars.get('lY'))
+    #add propensity for reaction 1:
+    pros.append(pars.get('gX')*sh_impact_Y)
+    #add propensity for reaction 2:
+    pros.append(pars.get('kX')*vars.get('X'))
+    #add propensity for reaction 3:
+    pros.append(pars.get('gY')*sh_impact_X)
+    #add propensity for reaction 4:
+    pros.append(pars.get('kY')*vars.get('Y'))
+    return pros 
 
 #-----------------------------------------------------------------------------#
-def perform_sp_reaction(X, Y, p1, p2, p3, p4):
+def perform_sp_reaction(vars, pros):
     '''
     This method stochastically chooses which reaction to occur and updates the 
     system after performing the chosen reaction.
     '''
-    pr=random.uniform(0,1)
-
-    if (pr<=p1):
-        X+=1
-    elif (pr<=(p1+p2)):
-        X-=1
-    elif (pr<=(p1+p2+p3)):
-        Y+=1
-    else: #(pr>(p1+p2+p3) and pr<=(p1+p2+p3+p4)):
-        Y-=1
-    #else:
-    #    pass 
-    return (X,Y) 
+    ptotal = sum(pros) #total propensity
+    pr = [x/ptotal for x in pros]
+    rn=random.uniform(0,1)
+    #choose reaction based on rn:
+    if (rn<=pr[0]):
+        vars['X']+=1
+    elif (rn<=(pr[0]+pr[1])):
+        vars['X']-=1
+    elif (rn<=(pr[0]+pr[1]+pr[2])):
+        vars['Y']+=1
+    else: #(rn>(pr[0]+pr[1]+pr[2]) and pr<=(pr[0]+pr[1]+pr[2]+pr[3])):
+        vars['Y']-=1
+    return vars 
 
 #-----------------------------------------------------------------------------#
-def run_simulation(X0,Y0,
-                   gX,gY,
-                   kX,kY,
-                   X,Y,
-                   nX,nY,
-                   lX,lY,
-                   tmax):
+def run_simulation(pars, vars, tmax):
     '''
     This method repeatedly performs the following tasks: 
     (1) Calculate propensities for each reaction as a function of the current
@@ -88,47 +109,75 @@ def run_simulation(X0,Y0,
     #initialize current time:
     tc=0
     #iteration counter:
-    cnt=0;
-    #initialize variables to save the trajectory: 
-    ser_X=[X]
-    ser_Y=[Y]
-    ser_cnt=[0]
-    ser_tm=[tc]
     factor=1000
     count = 0
     #run the simulation using Gillespie's Direct Method:
     while(tc<tmax):
         #calculate propensities:
-        pR1,pR2,pR3,pR4=calculate_propensities(X0,Y0,
-                                               gX,gY,
-                                               kX,kY,
-                                               X,Y,
-                                               nX,nY,
-                                               lX,lY)
-        Rtotal= pR1+pR2+pR3+pR4
-        if(not Rtotal):
+        pros = calculate_propensities(pars, vars)
+        ptotal=sum(pros) #total propensity
+        if(not ptotal):
             continue
-        #if (int(tc)/1000)
+        #save configuration at multiple of 'factor' timesteps:
         if (not (count%factor)):
-            print(tc, ' ', X, ' ', Y)
+            print(tc, ' ', vars.get('X'), ' ', vars.get('Y'))
+
         #perform specific reaction based on propensity values:
-
-        (X,Y)=perform_sp_reaction(X,Y,pR1/Rtotal,pR2/Rtotal,
-                                          pR3/Rtotal,pR4/Rtotal)
-        if(X<0 or Y<0):
-            return (ser_cnt,ser_X,ser_Y) 
-
-        #parameter for the exponential distribution:
-        lambd=Rtotal # lambd = 1/mean where mean = 1/Rtotal
+        vars=perform_sp_reaction(vars, pros)
+        if(vars.get('X')<0 or vars.get('Y')<0):
+            return None
+        #obtain wait time dt from exponential distribution:
+        lambd=ptotal # lambd = 1/mean where mean = 1/Rtotal
         dt=random.expovariate(lambd)
         tc+=dt
         count+=1
-        #cnt+=1
-        #store the state of the system:
-        #ser_X.append(X)
-        #ser_Y.append(Y)
-        #ser_cnt.append(cnt)
-    return ser_cnt,ser_X,ser_Y
+    return None
+
+def run_simulation_old(pars, vars, tmax):
+    '''
+    This method repeatedly performs the following tasks: 
+    (1) Calculate propensities for each reaction as a function of the current
+        state of the system.
+    (2) Stochastically chooses the wait time (dt) for the next reaction to 
+        occur.  
+    (3) Stochastically selects the next reaction to occur and update the 
+        system after the reaction is performed.
+    '''
+    import numpy as np
+
+    #initialize current time:
+    tc=0
+    #iteration counter:
+    factor=1000
+    count = 0
+    #run the simulation using Gillespie's Direct Method:
+    while(tc<tmax):
+        #calculate propensities:
+        pR1,pR2,pR3,pR4=calculate_propensities(pars, vars)
+        #pros = calculate_propensities(pars, vars)
+        Rtotal= pR1+pR2+pR3+pR4
+        #ptotal=sum(pros) #total propensity
+        #if(not ptotal):
+        if(not Rtotal):
+            continue
+        #save configuration at multiple of 'factor' timesteps:
+        if (not (count%factor)):
+            print(tc, ' ', vars.get('X'), ' ', vars.get('Y'))
+
+        #perform specific reaction based on propensity values:
+        vars=perform_sp_reaction_old(vars,pR1/Rtotal,pR2/Rtotal,
+                                      pR3/Rtotal,pR4/Rtotal)
+        #vars=perform_sp_reaction(vars, pros)
+
+        if(vars.get('X')<0 or vars.get('Y')<0):
+            return None
+        #obtain wait time dt from exponential distribution:
+        lambd=Rtotal # lambd = 1/mean where mean = 1/Rtotal
+        #lambd=ptotal # lambd = 1/mean where mean = 1/Rtotal
+        dt=random.expovariate(lambd)
+        tc+=dt
+        count+=1
+    return None
 
 #-----------------------------------------------------------------------------#
 def plot_trajectory(series_cnt, series_X, series_Y):
@@ -144,32 +193,9 @@ def plot_trajectory(series_cnt, series_X, series_Y):
 #-----------------------------------------------------------------------------#
 if __name__=="__main__":
     import time
-    #set system parameters:
-    #X0=100; Y0=100
-    #X0=30; Y0=30
-    #X0=20; Y0=20
-    X0=10; Y0=10
-    #gX=50; gY=50; #production rates for X and Y 
-    #gX=10; gY=10; #production rates for X and Y 
-    gX=5; gY=5; #production rates for X and Y 
-    kX=0.1; kY=0.1 #degradation rates for X and Y
-    nX=3.0; nY=3.0 #cooperativities of X and Y
-    lX=0.1; lY=0.1 #fold changes for the shifted Hill function
-
-    #set initial conditions:
-    X=100; Y=100
-    #X=30; Y=30
-    #set reaction time:
-    tmax=5000000
+    (pars,vars,tmax)=defineSystem()
     t_start=time.time()
-    #run simulation:
-    (ser_cnt,ser_X,ser_Y)=run_simulation(X0,Y0,
-                                           gX,gY,
-                                           kX,kY,
-                                           X,Y,
-                                           nX,nY,
-                                           lX,lY,
-                                           tmax)
+    run_simulation(pars, vars, tmax)
     t_end=time.time()
     print('simulation time: ', t_end-t_start)
     #plot_trajectory(ser_cnt,ser_X,ser_Y)
